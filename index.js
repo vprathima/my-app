cc81028a0452988b692fa178d325061ddb461033
vprathima <ext.valasapalli.prathima@mastery.net>
2025-07-09 19:19:54 +0530

    Update: Changed message to Hello

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, Arne! Node.js server is running 🚀\n");
+  res.end("Hello\n");
 });
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+
